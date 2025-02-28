1f2bfdfdf62c1487a265173969c05b609591ac59
oralxan <oralxan@users.noreply.github.com>
2025-02-28 21:41:40 +0500

    up

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -106,8 +106,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
